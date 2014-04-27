6545af9bf3c274b308f650eb3613a8611dd788e8
Kim Franken <scorpion919@gmail.com>
2014-04-27 15:48:54 +0200

    def gesamtpunkte

--- pytipp.py
+++ pytipp.py
@@ -1,6 +1,7 @@
 #Python Tippspiel
 
 Teilnehmer = ["Kim", "Thomas"]
+Spiele = [1, 2, 3] # Liste der Spiele
 
 def ergebnis(spiel):
 	a = [1,3] # nur zum testen manuell eingegeben
@@ -32,4 +33,14 @@ def punkte(spiel, spieler):
 	print p
 	return p	
 
+def gesamtpunkte(spieler):
+	p = 0
+	x = 0
+	sp = spieler
+	# def punkte fuer "spieler" fuer alle spiele auswerden und aufsummieren
+	for x in Spiele:
+		p += punkte(Spiele[x], sp)
+	return p
+
 punkte(3, 3) # noch keine sinnvolle uebergabe
+print len(Spiele)
